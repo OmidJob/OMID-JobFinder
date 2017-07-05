14b65de018b4611ddba2bf7500e05d9bfdc87ba4
miladranaei <ranaei.milad72@gmail.com>
2017-07-05 13:38:37 +0430

    میلاد رعنایی سیادت
    تغییر در فایل کانفیگ و اضافه کردن اسم milad ranaei

--- OMID/application/config/config.php
+++ OMID/application/config/config.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
+// milad ranaei
 /*
 |--------------------------------------------------------------------------
 | Base Site URL
